ff430ab740f844a268deb52b9379d20a5de24f05
fatemeh <fatemeh.ka84@gmail.com>
2021-07-26 16:20:45 -0400

    FIX deciaml BUTTON

--- app/src/main/java/com/fatemeh/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/fatemeh/calculatorapp/MainActivity.kt
@@ -8,6 +8,9 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric:Boolean=false
+    var lasDot:Boolean=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,10 +18,19 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         txtInput.append((view as Button).text)
+        lastNumeric=true
     }
     fun onClear(view: View){
         txtInput.text=""
+        lastNumeric=false
+        lasDot=false
+    }
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lasDot){
+            txtInput.append(".")
+            lastNumeric=false
+            lasDot=true
 
-
+        }
     }
 }
