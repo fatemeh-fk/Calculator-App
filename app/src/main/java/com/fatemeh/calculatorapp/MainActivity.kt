3f2ecd70c6a1dfeedf36a3b5520d5642a9ae564c
fatemeh <fatemeh.ka84@gmail.com>
2021-07-26 15:52:45 -0400

    FIX CLEAR BUTTON

--- app/src/main/java/com/fatemeh/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/fatemeh/calculatorapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.fatemeh.calculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this,"button works",Toast.LENGTH_SHORT).show()
+        txtInput.append((view as Button).text)
+    }
+    fun onClear(view: View){
+        txtInput.text=""
+
+
     }
 }
