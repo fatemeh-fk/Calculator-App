43bdef06e834b9b66147de0df6190e47aacdb522
fatemeh <fatemeh.ka84@gmail.com>
2021-07-26 21:36:42 -0400

    fix operation and finalizing the app

--- app/src/main/java/com/fatemeh/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/fatemeh/calculatorapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,11 +20,98 @@ class MainActivity : AppCompatActivity() {
     fun onDigit(view: View) {
         txtInput.append((view as Button).text)
         lastNumeric=true
+
+
     }
     fun onClear(view: View){
         txtInput.text=""
         lastNumeric=false
         lasDot=false
+    }
+    fun onOperator(view: View){
+        if(lastNumeric && !isOperatorAdded(txtInput.text.toString())){
+            txtInput.append((view as Button).text)
+            lastNumeric=false
+            lasDot=false
+        }
+    }
+    fun onEqeal(view: View){
+        if(lastNumeric){
+            var tvValue=txtInput.text.toString()
+            var prefix=""
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix="-"
+                    tvValue=tvValue.substring(1)
+                }
+                if(tvValue.contains("/")){
+                    val splitValue=tvValue.split("/")
+                    //99/1
+                    var left=splitValue[0]//99
+                    var right=splitValue[1]//1
+
+                    if(!prefix.isEmpty()){
+                        left=prefix+left
+
+                    }
+                    txtInput.text=removeZeroAfterDot((left.toDouble()/right.toDouble()).toString())
+                }
+                if(tvValue.contains("*")){
+                    val splitValue=tvValue.split("*")
+                    //99*1
+                    var left=splitValue[0]//99
+                    var right=splitValue[1]//1
+
+                    if(!prefix.isEmpty()){
+                        left=prefix+left
+
+                    }
+                    txtInput.text=removeZeroAfterDot((left.toDouble()*right.toDouble()).toString())
+                }
+                if(tvValue.contains("-")){
+                    val splitValue=tvValue.split("-")
+                    //99-1
+                    var left=splitValue[0]//99
+                    var right=splitValue[1]//1
+
+                    if(!prefix.isEmpty()){
+                        left=prefix+left
+
+                    }
+                    txtInput.text=removeZeroAfterDot((left.toDouble()-right.toDouble()).toString())
+                }
+                if(tvValue.contains("+")){
+                    val splitValue=tvValue.split("+")
+                    //99-1
+                    var left=splitValue[0]//99
+                    var right=splitValue[1]//1
+
+                    if(!prefix.isEmpty()){
+                        left=prefix+left
+
+                    }
+                    txtInput.text=removeZeroAfterDot((left.toDouble()+right.toDouble()).toString())
+                }
+
+            }catch (e:ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+    private fun removeZeroAfterDot(result:String):String{
+        var value=result
+        if(result.contains("0"))
+            value=result.substring(0,result.length-2);
+            return value
+
+    }
+    private fun isOperatorAdded(value :String):Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/")||value.contains("*")
+                    ||value.contains("+")||value.contains("-")
+        }
     }
     fun onDecimalPoint(view: View){
         if(lastNumeric && !lasDot){
